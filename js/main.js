2ba8215626c2150915b79dee2801e621df928f3c
RashadNovruzov <r4shad.novruz0v@gmail.com>
2022-07-17 11:50:30 +0400

    Section bestsellers is ready

--- js/main.js
+++ js/main.js
@@ -11,18 +11,3 @@ burger.addEventListener('click',function(){
 });
 
 
-var items = document.getElementsByClassName('explore__item');
-var objects_item = {};
-var texts = document.getElementsByClassName('explore-item__text');
-for (var i = 0; i < items.length; i++) {
-	items[i].addEventListener('click',function(){
-		var text = objects_item.this;
-		console.log(text);
-		if (this.style.backgroundColor!=="rgb(95, 215, 136)") {
-			this.style.backgroundColor="rgb(95, 215, 136)";
-		}else if(this.style.backgroundColor=="rgb(95, 215, 136)"){
-			this.style.backgroundColor="#F9FAFB";
-		}
-	})
-}
-
